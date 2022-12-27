1f6b06e27dde1b0e32f7afaed3cceb52437ebeb8
Olivier Forget <me@olivierforget.net>
2022-12-27 10:31:01 -0800

    Add ds-host docs and tweaks
    - add vue to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), vue()]
 });
---
+++ src/layouts/ValidVersion.vue
@@ -0,0 +1,57 @@
+<style scoped>
+h5 {
+	margin: 0 0 5px -35px;
+	text-transform: uppercase;
+	font-size: 1rem;
+	display: inline-block;
+	background-color: rgb(57, 78, 172);
+	color: white;
+	font-weight:bold;
+	padding: 2px 35px;
+}
+.valid-version {
+	background: linear-gradient(0deg, rgb(57, 78, 172) 0%, rgb(235, 54, 54) 50%, rgba(252,176,69,1) 100%);	/*rgba(131,58,180,1)*/
+	margin: 1em 0;
+}
+.valid-version > div {
+	margin-left: 10px;
+	padding: 10px;
+	background-color: white;
+}
+p {
+	margin: 0;
+}
+span.ver {
+    font-weight: bold;
+}
+</style>
+
+<script setup lang="ts">
+const props = defineProps<{
+  from?: string
+  to?: string
+}>();
+
+if( !props.from && !props.to ) throw new Error("no versions passed to ValidVersion");
+</script>
+
+<template>
+    <div class="valid-version">
+        <div>
+            <h5>Applicable Versions:</h5>
+            <p>
+                This page is valid for 
+                <template v-if="!!from && !!to">
+                    versions <span class="ver">{{ from }}</span> to <span class="ver">{{ to }}</span>.
+                </template>
+                <template v-else-if="!!from">
+                    version <span class="ver">{{ from }}</span> and above.
+                </template>
+                <template v-else-if="!!to">
+                    versions up to <span class="ver">{{ to }}</span>.
+                </template>
+                <template v-else></template>
+            </p>
+        </div>
+    </div>
+</template>
